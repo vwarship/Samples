038098ad02208b30d8a35c48e14912ee18e47d3f
vwarship <vwarship@163.com>
2015-02-17 23:05:31 +0800

    Android Sample : Download Text 下载文本文件

---
+++ Android/DownloadText/app/src/main/java/com/zaoqibu/downloadtext/MainActivity.java
@@ -0,0 +1,101 @@
+package com.zaoqibu.downloadtext;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new downloadTextTask().execute(makeURLStringWithGitHub("text.txt"));
+    }
+
+    private String makeURLStringWithGitHub(String filename) {
+        String gitHubURLFormat = "https://raw.githubusercontent.com/vwarship/Samples/master/Android/DownloadText/%s";
+        return String.format(gitHubURLFormat, filename);
+    }
+
+    private class downloadTextTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String text = "";
+
+            try {
+                text = downloadText(params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView textView = (TextView)findViewById(R.id.textView);
+            textView.setText(s);
+        }
+    }
+
+    private String downloadText(String urlString) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        URL url = new URL(urlString);
+        URLConnection urlConnection = url.openConnection();
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection)urlConnection;
+        httpURLConnection.setAllowUserInteraction(false);
+        httpURLConnection.setInstanceFollowRedirects(true);
+        httpURLConnection.setRequestMethod("GET");
+
+        if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            final int BUF_SIZE = 4096;
+            char[] buf = new char[BUF_SIZE];
+            int readedLen = 0;
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            while ((readedLen = reader.read(buf)) > 0) {
+                sb.append(new String(buf, 0, readedLen));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
