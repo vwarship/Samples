b0b53257815830165f522fdf04987b7a321e6bfc
vwarship <vwarship@163.com>
2015-02-21 01:16:58 +0800

    Android Sample : Bluetooth 增加了启动和关闭蓝牙的功能

--- Android/Bluetooth/app/src/main/java/com/zaoqibu/bluetooth/MainActivity.java
+++ Android/Bluetooth/app/src/main/java/com/zaoqibu/bluetooth/MainActivity.java
@@ -7,11 +7,15 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
     private BroadcastReceiver bluetoothStateChangeReceiver = null;
+    private static final int BLUETOOTH_ENABLE = 1;
+    private static final int BLUETOOTH_DISABLE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +37,8 @@ public class MainActivity extends ActionBarActivity {
     private void initBluetoothUI() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        TextView tvBluetoothState = (TextView)findViewById(R.id.tvBluetoothState);
-        tvBluetoothState.setText(bluetoothAdapter.isEnabled() ? "On" : "Off");
+        Switch switchBluetooth = (Switch)findViewById(R.id.switchBluetooth);
+        switchBluetooth.setChecked(bluetoothAdapter.isEnabled());
 
         TextView tvAddress = (TextView)findViewById(R.id.tvAddress);
         tvAddress.setText(bluetoothAdapter.getAddress());
@@ -42,6 +46,25 @@ public class MainActivity extends ActionBarActivity {
         if (bluetoothAdapter.isEnabled()) {
             TextView tvName = (TextView)findViewById(R.id.tvName);
             tvName.setText(bluetoothAdapter.getName());
+        }
+    }
+
+    public void onClickSwitchBluetooth(View view) {
+        Switch switchBluetooth = (Switch)findViewById(R.id.switchBluetooth);
+
+        if (switchBluetooth.isChecked()) {
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), BLUETOOTH_ENABLE);
+        }
+        else {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.disable();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BLUETOOTH_ENABLE && resultCode == RESULT_OK) {
+            initBluetoothUI();
         }
     }
 
