ce89fe8b44710a710ca3fe46207628a92b4a5cd9
vwarship <vwarship@163.com>
2015-02-22 01:37:42 +0800

    Android Sample : Resource 增加 Quantity Strings 和 String Escaping

--- Android/Resource/app/src/main/java/com/zaoqibu/resource/ResourceStringTypeActivity.java
+++ Android/Resource/app/src/main/java/com/zaoqibu/resource/ResourceStringTypeActivity.java
@@ -1,5 +1,6 @@
 package com.zaoqibu.resource;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
@@ -12,11 +13,15 @@ public class ResourceStringTypeActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resource_string_type);
 
+        Resources resources = getResources();
+
+        // Simple String
         String simpleString = getString(R.string.hello);
         TextView tvSimpleString = (TextView)findViewById(R.id.tvSimpleString);
         tvSimpleString.setText(simpleString);
 
-        String[] stringArray = getResources().getStringArray(R.array.rgb);
+        // String Array
+        String[] stringArray = resources.getStringArray(R.array.rgb);
         StringBuilder sb = new StringBuilder("String Array: RGB(");
         for (String s : stringArray)
             sb.append(s).append(" ");
@@ -24,6 +29,15 @@ public class ResourceStringTypeActivity extends ActionBarActivity {
 
         TextView tvStringArray = (TextView)findViewById(R.id.tvStringArray);
         tvStringArray.setText(sb.toString());
+
+        // Quantity Strings(Plurals)
+        TextView tvPlurals = (TextView)findViewById(R.id.tvPlurals);
+        tvPlurals.setText("Plurals: " + resources.getQuantityString(R.plurals.numberOfSongsAvailable, 1, 1) + "\t" +
+                resources.getQuantityString(R.plurals.numberOfSongsAvailable, 10, 10));
+
+        // String Escaping ' "
+        TextView tvEscaping = (TextView)findViewById(R.id.tvEscaping);
+        tvEscaping.setText(getString(R.string.escaping));
     }
 
 }
