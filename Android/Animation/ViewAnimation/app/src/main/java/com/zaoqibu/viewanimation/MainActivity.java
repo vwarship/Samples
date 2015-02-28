971a63ac5db9ec0f1f7a2cf9a8f5be0749983493
vwarship <vwarship@163.com>
2015-02-28 23:57:55 +0800

    Android Sample : View Tween Animation

---
+++ Android/Animation/ViewAnimation/app/src/main/java/com/zaoqibu/viewanimation/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zaoqibu.viewanimation;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        image.startAnimation(hyperspaceJump);    }
+
+}
