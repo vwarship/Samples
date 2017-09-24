1e86da378fb1534f158618be9d2b20990040a1cb
Junjian Wang <vwarship@163.com>
2017-09-25 03:48:19 +0800

    自动布局

---
+++ Swift/SimpleCenter/SimpleCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCenter
+//
+//  Created by 王军建 on 2017/9/24.
+//  Copyright © 2017年 狗吃草. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
