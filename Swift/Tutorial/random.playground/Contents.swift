8ac73052a385414cfd122049321796c4bc5065d1
Junjian Wang <vwarship@163.com>
2017-09-26 20:30:16 +0800

    random
    GKRandomDistribution

---
+++ Swift/Tutorial/random.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import GameplayKit
+
+let random = GKRandomDistribution(lowestValue: 1, highestValue: 10)
+
+print(random.nextInt())
+print(random.nextInt())
+print(random.nextInt())
