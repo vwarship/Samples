08e5a89d7414344d65371240ef7c21299ead5256
Junjian Wang <vwarship@163.com>
2017-11-03 06:54:11 +0800

    短信过滤
    ILMessageFilterExtension

---
+++ iOS/Practice/SMSFilter/SMSFilter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SMSFilter
+//
+//  Created by 王军建 on 2017/11/2.
+//  Copyright © 2017年 狗吃草. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
